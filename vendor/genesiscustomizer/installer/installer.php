e3fe7524aac5bdc1b93be64f6a1ebd5ac0d7661c
seothemes <seothemeswp@gmail.com>
2019-03-31 19:59:55 +0800

    Update installer for 0.1.15

--- vendor/genesiscustomizer/installer/installer.php
+++ vendor/genesiscustomizer/installer/installer.php
@@ -2,4 +2,19 @@
 
 namespace GenesisCustomizer;
 
+// Install and active dependencies.
 \WP_Dependency_Installer::instance()->run( __DIR__ );
+
+add_action( 'admin_init', __NAMESPACE__ . '\merlin_redirect' );
+/**
+ * Redirect on theme activation.
+ *
+ * @return void
+ */
+function merlin_redirect() {
+	global $pagenow;
+
+	if ( "themes.php" == $pagenow && is_admin() && isset( $_GET['activated'] ) ) {
+		exit( wp_redirect( admin_url( 'admin.php?page=genesis-customizer-setup' ) ) );
+	}
+}
